dc4efee67dd07df63cec1753f610821dc8443578
Shyam-25 <shyam.s@optisolbusiness.com>
2020-12-28 15:37:27 +0530

    first

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {  View, Text } from 'react-native';
+
+export default class App extends Component
+{
+  constructor(){
+    super(props);
+  }
+  render()
+  {
+    return(
+      <View>
+        <Text>HI</Text>
+      </View>
+    );
+  }
+}
+
